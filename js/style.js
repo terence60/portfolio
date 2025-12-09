0436f9fcea17c455eefa8e64e15b850748056a79
Terence Dinguilu <terence.dinguilu@gmail.com>
2025-12-09 21:11:34 +0100

    sixth commit

--- js/style.js
+++ js/style.js
@@ -73,7 +73,7 @@ let objet = {
     
 //}
 const myFunction = (item, item2) => {
-    console.log(item,item2);
+    //console.log(item,item2);
     
 }
 
@@ -86,43 +86,136 @@ const calcul = (nb1, nb2) => {
 }
 
 let result = calcul(4,5);
-console.log(result);
+//console.log(result);
 
 // interagir avec le dom // methode, propriete, evenement
 
-console.log(document.body);
+//console.log(document.body);
 
 // selectors
 
-let header = document.querySelector(".header");
+// let header = document.querySelector(".header");
 
 // console.log(header);
 
-let grids = document.querySelectorAll('.grid');
-console.log(grids);
+// let grids = document.querySelectorAll('.grid');
+// console.log(grids);
 
-grids.forEach(grid => {
-    (grid.classList.add('titi'));
-    console.log(grid);
+//grids.forEach(grid => {
+    //(grid.classList.add('titi'));
+    //console.log(grid);
     
-});
+// });
 
 // evenements les plus courants 
 
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM chargé");
-});
+// document.addEventListener("DOMContentLoaded", () => {
+   // console.log("DOM chargé");
+// });
 
-header.addEventListener("mouseenter", (e) => {
-    console.log("souris entre");
-});
+//header.addEventListener("mouseenter", (e) => {
+   // console.log("souris entre");
+//});
 
 // insertion dom et navigation dans le dom
 
 let div = document.createElement("div");
 div.classList.add("top");
 div.innerHTML = `<span>Top zone</span>`
-header.parentNode.append(div);
+//console.log(header.nextElementSibling);
+
+
+// function 
+
+function menuMobile() {
+    const btn = document.querySelector('.burger');
+    const header = document.querySelector('.header');
+    const links = document.querySelectorAll('.navbar a');
+
+    btn.addEventListener('click' , () => {
+       header.classList.toggle('animation-nav');
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click' , () => {
+        header.classList.remove('animation-nav');
+        });
+    });
+
+}
+menuMobile();
+
+/* Portfolio */
+
+function tabsFilters() {
+    const tabs = document.querySelectorAll('.portfolio-filters a');
+    const projets = document.querySelectorAll('.portfolio .card');
+    
+    const showProjets = (elem) => {
+        console.log(elem);
+        
+        projets.forEach(projet => {
+            // console.log(projet);
+        });
+    }
+  
+    tabs.forEach (elem => {
+        elem.addEventListener('click', (event) => {
+        event.preventDefault();
+        let filter = elem.getAttribute('data-filter');
+        // console.log(filter);
+        showProjets(filter)
+        elem.classList.add('active');
+        });
+    })
+}
+
+// effets
+
+const observerIntersectionAnimation = () => {
+  const sections = document.querySelectorAll('section');
+  const skills = document.querySelectorAll('.competences .bar');
+
+  sections.forEach((section, index) => {
+    if (index === 0) return;
+    section.style.opacity = "0";
+    section.style.transition = "all 1.6s";
+  });
+
+  skills.forEach((elem, index) => {
+
+    elem.style.width = "0";
+    elem.style.transition = "all 1.6s";
+  });
+
+  let sectionObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        elem.style.opacity = 1;
+      }
+    });
+  });
+
+  sections.forEach(section => {
+    sectionObserver.observe(section);
+  });
+
+  let skillsObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        elem.style.width = elem.dataset.width + '%';
+      }
+    });
+  });
+
+  skills.forEach(skill => {
+    skillsObserver.observe(skill);
+  });
+}
+
+observerIntersectionAnimation();
 
 
 
